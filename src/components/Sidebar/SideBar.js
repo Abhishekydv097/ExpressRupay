95bc3ddb9a8630e24e5255c4be872a53ee59b34c
Abhishekydv097 <abhishekyadav20011@gmail.com>
2023-06-23 16:19:36 +0530

    first commit

--- src/components/Sidebar/SideBar.js
+++ src/components/Sidebar/SideBar.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import './SideBar.css';
+import Dashboard from './icons/Dashboard.png'
+import Leads from './icons/Leads.png'
+import LeadsUpdate from './icons/LeadsUpdate.png'
+import LeadsReminder from './icons/LeadsReminder.png'
+import Calendar from './icons/Calender.png'
+import About from './icons/About.png'
 
 const Sidebar = ({ isOpen }) => {
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
       <div className="sidebar-content">
         <button className="sidebar-button">
-          <span className="sidebar-icon">Dashboard</span>
+          <span className="dashboard">Dashboard</span>
         </button>
         <button className="sidebar-button">
-          <span className="sidebar-icon">Leads</span>
+        <img src={Leads} alt='Dashboard Icon' className='sidebar-icon'/>
+        <span>Leads</span>
         </button>
         <button className="sidebar-button">
-          <span className="sidebar-icon">Leads Status Update</span>
+        <img src={LeadsUpdate} alt='Dashboard Icon' className='sidebar-icon'/>
+        <span>Leads Status Update</span>
         </button>
         <button className="sidebar-button">
-          <span className="sidebar-icon">Leads Reminder</span>
+        <img src={LeadsReminder} alt='Leads Remider' className='sidebar-icon'/>
+        <span>Leads Reminder</span>
         </button>
         <button className="sidebar-button">
-          <span className="sidebar-icon">Calendar</span>
+        <img src={Calendar} alt='Dashboard Icon' className='sidebar-icon'/>
+        <span>Calendar</span>
         </button>
         <button className="sidebar-button">
-          <span className="sidebar-icon">About</span>
+        <img src={About} alt='About' className='sidebar-icon'/>
+        <span>About</span>
+        </button>
+        <button className="sidebar-button">
+          <img src={Dashboard} alt='Dashboard Icon' className='sidebar-icon'/>
+          <span>Dashboard</span>
         </button>
       </div>
     </div>
